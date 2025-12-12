d722c784a7b4a38b2fe08a23f9bd4d421f537414
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2025-12-12 13:42:11 +0500

    Create main.py

---
+++ Advent_Of_Code_2025/Day_12/main.py
@@ -0,0 +1,32 @@
+D = open("input.txt").read()
+
+parts = D.split('\n\n')
+presents = parts[:-1]
+SIZES = {}
+for present in presents:
+    lines = present.splitlines()
+    name = int(lines[0][:-1])
+    G = [list(row) for row in lines[1:]]
+    size = 0
+    for row in G:
+        for c in row:
+            if c=='#':
+                size += 1
+    SIZES[name] = size
+
+ans = 0
+regions = parts[-1]
+for region in regions.splitlines():
+    sz, ns = region.split(': ')
+    R,C = [int(x) for x in sz.split('x')]
+    ns = [int(x) for x in ns.split()]
+    total_present_size = sum(n*SIZES[i] for i,n in enumerate(ns))
+    total_grid_size = R*C
+    if total_present_size * 1.3 < total_grid_size:
+        ans += 1
+    elif total_present_size > total_grid_size:
+        pass
+    else:
+        print(f'HARD {total_grid_size=} {total_present_size=}')
+
+print(F"Answer : {ans}")
